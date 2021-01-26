7a7c802db5b7764ef12189d3bfe0e9a3de2f233f
sohanpaliyal <sohnpaliyal@gmail.com>
2021-01-26 21:41:43 +0530

    Added more practice files

---
+++ iip acadamy/layouts/footer.php
@@ -0,0 +1,55 @@
+		<div class="footer">
+			<div class="footer-top-container">
+				<div class="quick-links-container">
+					<h2>Quick Links</h2>
+					<ul>
+						<li><a href="home.html" class="anchor">Home</a></li>
+						<li><a href="About.html" class="anchor">About Us</a></li>
+						<li><a href="courses.html" class="anchor">Courses</a></li>
+						<li><a href="gallery.html" class="anchor">Gallery</a></li>
+						<li><a href="enquiry.html" class="anchor">Enquiry</a></li>
+						<li><a href="contact.html" class="anchor">Contact Us</a></li>
+					</ul>
+				</div>
+				<div class="footer-train-container">
+					<h2>Training</h2>
+					<div class="train-cont">
+						<ul>
+							<li><a href="" class="anchor" title="PHP(Web Development)">PHP(Web Development)</a></li>
+							<li><a href="" class="anchor" title="Advance PHP">Advance PHP</a></li>
+							<li><a href="" class="anchor" title="Python">Python</a></li>
+							<li><a href="" class="anchor" title="Android APP Development">Android App Development</a></li>
+							<li><a href="" class="anchor" title="Digital Marketing">Digital Marketing</a></li>
+						</ul>
+					</div>
+					<div class="train-cont">
+						<ul>
+							<li><a href="" class="anchor" title="Graphic Design">Graphic Design</a></li>
+							<li><a href="" class="anchor" title="Search Engine Optimization">Search Engine Optimization</a></li>
+							<li><a href="" class="anchor" title="Wordpress">Wordpress</a></li>
+							<li><a href="" class="anchor" title="Web design">Web design</a></li>
+							<li><a href="" class="anchor" title="Java">Java</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer-contact-container">
+					<h2>Contact info:</h2>
+					<ul class="cont-details">
+						<li>Call At: 9269698122</li>
+						<li>1st floor, Laxmi Tower, Bhaskar Circle, Ratanda, Raj - Jodhpur(342001)</li>
+					</ul>
+					<ul class="social-links">
+						<li><a href="https://www.instagram.com/wscubetechindia"><img src="assets/images/instagram.png"></a></li>
+						<li><a href="https://twitter.com/wscube"><img src="assets/images/twitter.png"></a></li>
+						<li><a href="https://www.facebook.com/wscubetech.india"><img src="assets/images/facebook.png"></a></li>
+						<li><a href="https://www.linkedin.com/company/wscube-tech"><img src="assets/images/linked-in.png"></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="copy-right">
+				Copyright © 2019
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ iip acadamy/layouts/header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>iip Home</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/custom.css"/>
+</head>
+<body>
+	<div id="main">
+		<div id="top-contact">
+			<div class="left-cont">
+				<img src="assets/images/web.png" class="left-top-pic" />
+				<div class="left-top-iip">
+					<a href="index.html" class="anchor">www.iipacademy.com</a>
+				</div>
+			</div>
+			<div class="right-cont">
+				<img src="assets/images/info.png" class="right-top-pic" />
+				<div class="right-top-info">
+					<a href="mailto:info@iipacademy.com" class="anchor">info@iipacademy.com</a>
+				</div>
+			</div>
+		</div>
+		<div class="menu-container">
+			<div class="left-menu-logo-div">
+				<a href="index.html">
+					<img src="assets/images/logo.png" class="logo">
+				</a>
+			</div>
+			<div class="menu-bar">
+				<div class="menu-item active"><a href="index.html" class="anchor">Home</a></div>
+				<div class="menu-item"><a href="about.html" class="anchor">About Us</a></div>
+				<div class="menu-item"><a href="courses.html" class="anchor">Courses</a></div>
+				<div class="menu-item"><a href="gallery.html" class="anchor">Gallery</a></div>
+				<div class="menu-item"><a href="enquiry.html" class="anchor">Enquiry</a></div>
+				<div class="menu-item"><a href="contact.html" class="anchor">Contact Us</a></div>
+			</div>
+		</div>
+		<!-- middle container -->
+		<div class="middle-container">
+			<!-- left panel of middle container starts here -->
+			<div class="middle-left">
+				<!-- left menu starts here -->
+				<div class="menu-item-left">
+					<a href="index.html" class="anchor menu-item-left-a">Home</a>
+				</div>
+				<div class="menu-item-left">
+					<a href="about.html" class="anchor menu-item-left-a">About Us</a>
+				</div>
+				<div class="menu-item-left">
+					<a href="courses.html" class="anchor menu-item-left-a">Courses</a>
+				</div>
+				<div class="menu-item-left">
+					<a href="gallery.html" class="anchor menu-item-left-a">Gallery</a>
+				</div>
+				<div class="menu-item-left">
+					<a href="enquiry.html" class="anchor menu-item-left-a">Enquiry</a>
+				</div>
+				<div class="menu-item-left">
+					<a href="contact.html" class="anchor menu-item-left-a">Contact Us</a>
+				</div>	
+				<!-- /left menu ends here -->
+				<!-- left panel's news bar starts here -->
+				<div class="middle-left-buttom">
+					<div class="middle-left-buttom-news">
+						News
+					</div>
+					<ul class="news-list">
+						<li class="news-list-li"><img src="assets/images/dot.jpg" class="news-list-li-img"> ipsum dolor sit amet, </li>
+						<li class="news-list-li"><img src="assets/images/dot.jpg" class="news-list-li-img"> ipsum dolor sit amet, </li>
+						<li class="news-list-li"><img src="assets/images/dot.jpg" class="news-list-li-img"> ipsum dolor sit amet, </li>
+						<li class="news-list-li"><img src="assets/images/dot.jpg" class="news-list-li-img"> ipsum dolor sit amet, </li>
+						<li class="news-list-li"><img src="assets/images/dot.jpg" class="news-list-li-img"> ipsum dolor sit amet, </li>
+					</ul>
+				</div>
+				<!-- /left panel's news bar ends here -->
+			</div>
+			<!-- /left panel of middle container ends here -->
